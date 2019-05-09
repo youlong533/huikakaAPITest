44b4ce3c5e369b78bbe57fe7a1cbe4ed93c03651
youlong <you533@qq.com>
2019-05-09 14:59:17 +0800

    修改运行脚本
    修改运行脚本

--- huikaka_API/TestCase_Lib/run_all.py
+++ huikaka_API/TestCase_Lib/run_all.py
@@ -4,7 +4,7 @@ import os
 lst = os.listdir(os.getcwd())
 
 for c in lst:
-    if os.path.isfile(c) and c.endswith('.py') and c.find("__main__")==-1:
+    if os.path.isfile(c) and c.endswith('.py') and c.find("run_all")==-1:
         print(c)
         #os.system(os.path.join(os.getcwd(),c))
         os.system("python ./%s" % c)
